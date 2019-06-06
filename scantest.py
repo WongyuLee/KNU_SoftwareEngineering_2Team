f1891567c8a19e574d081ac5ece973671b762263
seokgiLee <plot_lee@naver.com>
2019-06-06 19:13:59 +0900

    Update scantest.py
    오류수정

--- scantest.py
+++ scantest.py
@@ -53,8 +53,66 @@ if __name__=='__main__':
             print beacon
                     if _uuid == '11111111111111111111111111111113':
                             del returnedList[:]
-		    del uuidList[:]
-		    print "find"
-		    time.sleep(5)
-    sc.join()
+		    del uuidList[:]# test BLE Scanning software
+# jcs 6/8/2014
+
+from threading import Thread
+import blescan
+import sys
+import time
+
+import bluetooth._bluetooth as bluez
+
+dev_id = 0
+try:
+        sock = bluez.hci_open_dev(dev_id)
+        print "ble thread started"
+
+except:
+        print "error accessing bluetooth device..."
+        sys.exit(1)
+
+blescan.hci_le_set_scan_parameters(sock)
+blescan.hci_enable_le_scan(sock)
+global returnedList
+global uuidList
+returnedList=[]
+uuidList=[]
+
+def scan():
+    while True:
+        List = blescan.parse_events(sock, 5)
+            for beacon in List:
+                    beacon_data = beacon.split(',')
+                    _uuid = beacon_data[1]
+                    _major = beacon_data[2]
+                    _minor = beacon_data[3]
+                    _tx_power = beacon_data[4]
+            if _uuid in uuidList:
+                            continue
+                    else:
+                            returnedList.append(beacon)
+                            uuidList.append(_uuid)
+            if _uuid == '11111111111111111111111111111113': 
+                                time.sleep(5)
+	#uuid를 찾을때 scan을 정지시키지 않으면 scan이 계속 혼자 계속작동해서 이상하게 실행된다
+
+if __name__=='__main__':
+    sc = Thread(target = scan, args=())
+    sc.start()
+    while True:
+            print "----------"
+            for beacon in returnedList:
+                    beacon_data = beacon.split(',')
+                    _uuid = beacon_data[1]
+                    _major = beacon_data[2]
+                    _minor = beacon_data[3]
+                    _tx_power = beacon_data[4]
+            print beacon
+                    if _uuid == '11111111111111111111111111111113':
+                                del returnedList[:]
+                                del uuidList[:]
+                                print "find"
+                                time.sleep(5)
+                                break
 
