1a50720e6f82007878070ee0597fb035daa583c6
seokgiLee <plot_lee@naver.com>
2019-06-06 19:15:31 +0900

    Update electric_control.py
    beacon scan에 멀티쓰레딩 추가

--- electric_control.py
+++ electric_control.py
@@ -1,6 +1,7 @@
 # test BLE Scanning software
 # jcs 6/8/2014
 
+from threading import Thread
 import blescan
 import sys
 import RPi.GPIO as GPIO
@@ -13,43 +14,57 @@ GPIO.setup(21, GPIO.OUT)
 
 dev_id = 0
 try:
-	sock = bluez.hci_open_dev(dev_id)
-	print "ble thread started"
+    sock = bluez.hci_open_dev(dev_id)
+    print "ble thread started"
 
 except:
-	print "error accessing bluetooth device..."
-	sys.exit(1)
+    print "error accessing bluetooth device..."
+    sys.exit(1)
 
 blescan.hci_le_set_scan_parameters(sock)
 blescan.hci_enable_le_scan(sock)
+global returnedList
+global uuidList
 returnedList=[]
 uuidList=[]
 
-while True:
-	GPIO.output(21, True)
-	print "off"
-	List = blescan.parse_events(sock, 1)
-        for beacon in List:
-                beacon_data = beacon.split(',')
-                _uuid = beacon_data[1]
-                _major = beacon_data[2]
-                _minor = beacon_data[3]
-                _tx_power = beacon_data[4]
-                if _uuid in uuidList:
-                        continue
-                else:
-                        returnedList.extend(List)
-                        uuidList.append(_uuid)
+def scan():
+        while True:
+                List = blescan.parse_events(sock, 5)
+                for beacon in List:
+                        beacon_data = beacon.split(',')
+                        _uuid = beacon_data[1]
+                        _major = beacon_data[2]
+                        _minor = beacon_data[3]
+                        _tx_power = beacon_data[4]
+                        if _uuid in uuidList:
+                                continue
+                        else:
+                                returnedList.append(beacon)
+                uuidList.append(_uuid)
+            if _uuid == '11111111111111111111111111111113': 
+                                time.sleep(5)
 
-	for beacon in returnedList:
-		beacon_data = beacon.split(',')
-		_uuid = beacon_data[1]
-		_major = beacon_data[2]
-		_minor = beacon_data[3]
-		_tx_power = beacon_data[4]
-		if _uuid == '11111111111111111111111111111113':
-			GPIO.output(21, False)
-			print "on"
-			time.sleep(5)
-			GPIO.output(21,True)
-			time.sleep(10)
+if __name__=='__main__':
+    sc = Thread(target = scan, args=())
+        sc.start()
+    while True:
+        print 'start'
+        GPIO.output(21, True)
+        for beacon in returnedList:
+            beacon_data = beacon.split(',')
+            _uuid = beacon_data[1]
+            _major = beacon_data[2]
+            _minor = beacon_data[3]
+            _tx_power = beacon_data[4]
+            if _uuid == '11111111111111111111111111111113':
+                                del returnedList[:]
+                                del uuidList[:]
+                                print 'on'
+                                GPIO.output(21, False)
+                                time.sleep(5)
+                                print 'off'
+                                GPIO.output(21,True)
+                                time.sleep(5)
+                                break
+
