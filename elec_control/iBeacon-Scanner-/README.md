563d236f71c0ede0db0ef99e1dcdcbfcdcd73b14
cheesecat47 <cheesecat47@gmail.com>
2019-07-24 00:11:35 +0900

    Fix: Modify electric_control.py
    Modified python files - print functions are written in python2 style.
    So I changed them to python3 style.
    
    And add a test() function at the front of the main.
    If input is 'y', the circuit connects and the light on.
    Else if 'n', it makes the light off.
    Else if 'b', it goes to next code and waiting for the beacon signal.

--- elec_control/iBeacon-Scanner-/README.md
+++ elec_control/iBeacon-Scanner-/README.md
@@ -6,3 +6,9 @@ blescanner is a python program designed to read iBeacon advertizments using a Bl
 To test, "sudo python testblescanner.py"
 
 
+
+SSLAB, KNU
+July 2019
+
+If there's no bluetooth module, try this.
+https://stackoverflow.com/questions/23985163/python3-error-no-module-named-bluetooth-on-linux-mint
--- elec_control/iBeacon-Scanner-/electric_control.py
+++ elec_control/iBeacon-Scanner-/electric_control.py
@@ -16,10 +16,10 @@ dev_id = 0
 lock = threading.Lock()
 try:
     sock = bluez.hci_open_dev(dev_id)
-    print "ble thread started"
+    print("ble thread started")
 
 except:
-    print "error accessing bluetooth device..."
+    print("error accessing bluetooth device...")
     sys.exit(1)
 
 blescan.hci_le_set_scan_parameters(sock)
@@ -29,29 +29,56 @@ global uuidList
 returnedList=[]
 uuidList=[]
 
+
 def scan():
+	print('scan()')
 	while True:
 		List = blescan.parse_events(sock, 5)
+		print('List: {}'.format(List))
 		for beacon in List:
 			beacon_data = beacon.split(',')
 			_uuid = beacon_data[1]
 			_major = beacon_data[2]
 			_minor = beacon_data[3]
 			_tx_power = beacon_data[4]
+			
+			print('uuid: {}'.format(_uuid))
 			if _uuid in uuidList:
 				continue
 			else:
 				returnedList.append(beacon)
 				uuidList.append(_uuid)
-			if _uuid == '08ffffff000102030408163412030102':
+			# if _uuid == '08ffffff000102030408163412030102':
+			if _uuid == '012f4bf49e724fbe8d5fdcfb07954260':
+			# if _uuid == '0000992500001000800000805F9B34FB':
 				del uuidList[:]
 				lock.acquire()
 
+
+def test():
+	while True:
+		mg = input('y, n, b >> ')
+		if mg == 'y':
+			print('on')
+			GPIO.output(21, False)
+			continue
+		elif mg == 'n':
+			print('off')
+			GPIO.output(21, True)
+			continue
+		elif mg == 'b':
+			return
+		else:
+			sys.exit()
+
+
 if __name__=='__main__':
+	test()
+
 	sc = threading.Thread(target = scan, args=())
 	sc.start()
 	while True:
-		print 'start'
+		# print('start', end=' ')
 		GPIO.output(21, True)
 		for beacon in returnedList:
 			beacon_data = beacon.split(',')
@@ -59,15 +86,17 @@ if __name__=='__main__':
 			_major = beacon_data[2]
 			_minor = beacon_data[3]
 			_tx_power = beacon_data[4]
-			print beacon
-			if _uuid == '08ffffff000102030408163412030102':
-				print 'on'
+			print(beacon)
+			print('uuid: {}'.format(_uuid))
+			# if _uuid == '08ffffff000102030408163412030102':
+			if _uuid == '012f4bf49e724fbe8d5fdcfb07954260':
+			# if _uuid == '0000992500001000800000805F9B34FB':
+				print('on')
 				GPIO.output(21, False)
 				time.sleep(5)
-				print 'off'
+				print('off')
 				GPIO.output(21,True)
 				time.sleep(5)
 				del returnedList[:]
 				lock.release()
 				break
-
